6cbeaee1f337f1c4a2b856d33d328d9f78600b01
pablonicoletti123 <112667095+pablonicoletti123@users.noreply.github.com>
2023-02-23 21:48:25 -0300

    .

---
+++ index.php
@@ -0,0 +1,384 @@
+<!doctype html>
+
+<html lang="pt-br">
+
+
+
+<head>
+
+       
+    <meta charset="utf-8">
+
+       
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Cinema</title>
+
+       
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+
+
+
+<body>
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+                <div class="container-fluid">
+
+                        <a class="navbar-brand" href="#"><img src="img/loo.png" height="70"></a>
+
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+                                <span class="navbar-toggler-icon"></span>
+
+                            </button>
+
+                        <div class="collapse navbar-collapse" id="navbarNav">
+
+                                <ul class="navbar-nav">
+
+                                        <li class="nav-item">
+
+                                                <a class="nav-link active" aria-current="page" href="#">HOME</a>
+
+                                            </li>
+
+                                        <li class="nav-item">
+
+                                                <a class="nav-link" href="#">PRODUTOS</a>
+
+                                            </li>
+
+                                        <li class="nav-item">
+
+                                                <a class="nav-link" href="#">CADASTRO</a>
+
+                                            </li>
+
+                                        <li class="nav-item">
+
+                                                <a class="nav-link">CONTATO</a>
+
+                                            </li>
+
+                                    </ul>
+
+                            </div>
+
+                    </div>
+
+            </nav>
+
+
+
+        <div id="carouselExample" class="carousel slide">
+
+                <div class="carousel-inner">
+
+                        <div class="carousel-item active">
+
+                                <img src="img/foto01.jpg" class="d-block w-100">
+
+                            </div>
+
+                        <div class="carousel-item">
+
+                                <img src="img/foto02.jpg" class="d-block w-100">
+
+                            </div>
+
+                        <div class="carousel-item">
+
+                                <img src="img/foto03.jpg" class="d-block w-100">
+
+                            </div>
+
+                        <div class="carousel-item">
+
+                                <img src="img/foto04.jpg" class="d-block w-100">
+
+                            </div>
+
+                        <div class="carousel-item">
+
+                                <img src="img/foto05.jpg" class="d-block w-100">
+
+                            </div>
+
+                    </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+
+                        <span class="visually-hidden">Previous</span>
+
+                    </button>
+
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+
+                        <span class="visually-hidden">Next</span>
+
+                    </button>
+
+            </div>
+
+
+
+        <main class="container mt-5">
+
+                <div class="row">
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto03.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto04.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto05.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto06.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                    </div>
+
+
+
+                <div class="row mt-5">
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto07.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto08.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto10.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                        <div class="col">
+
+                                <div class="card">
+
+                                        <img src="img/card/foto03.jpg" class="card-img-top">
+
+                                        <div class="card-body">
+
+                                                <h5 class="card-title">Card title</h5>
+
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                                            </div>
+
+                                    </div>
+
+                            </div>
+
+                    </div>
+
+
+
+                <div class="row mt-5 bg-light">
+
+                        <div class="col">
+
+                                <h2 class="display-5 text-center">CADASTRE-SE</h2>
+
+                            </div>
+
+                    </div>
+
+                <div class="row bg-light">
+
+                        <div class="col">
+
+                                <div class="mb-3">
+
+                                        <label for="nome" class="form-label">Nome</label>
+
+                                        <input type="text" class="form-control" id="nome">
+
+                                    </div>
+
+                                <div class="mb-3">
+
+                                        <label for="email" class="form-label">E-mail</label>
+
+                                        <input type="email" class="form-control" id="email">
+
+                                    </div>
+
+                                <div class="mb-3">
+
+                                        <label for="cidade" class="form-label">Cidade</label>
+
+                                        <input type="text" class="form-control" id="cidade">
+
+                                    </div>
+
+                                <button class="btn btn-lg btn-primary">SALVAR</button>
+
+                            </div>
+
+                    </div>
+
+
+
+                <div class="row mt-5">
+
+                        <div class="col">
+
+                                <h2 class="display-5 text-center">CONTATO</h2>
+
+                            </div>
+
+                    </div>
+
+                <div class="row">
+
+                        <div class="col">
+
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.328661913478!2d-47.33128804932392!3d-22.75318113805539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1677199126960!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                            </div>
+
+                    </div>
+
+
+
+            </main>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</body>
+
+
+
+</html>
